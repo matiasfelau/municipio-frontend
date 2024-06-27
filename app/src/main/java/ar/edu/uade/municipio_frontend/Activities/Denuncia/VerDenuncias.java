44bf08976d485bd101383ddb8fa48a66b9c87f55
Maxmart27 <Maximmart007@gmail.com>
2024-06-27 08:17:46 -0300

    Create VerDenuncias.java

---
+++ app/src/main/java/ar/edu/uade/municipio_frontend/Activities/Denuncia/VerDenuncias.java
@@ -0,0 +1,35 @@
+package ar.edu.uade.municipio_frontend.Activities.Denuncia;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import ar.edu.uade.municipio_frontend.R;
+
+public class VerDenuncias extends AppCompatActivity {
+    //inicializaciones
+    EditText inputId;
+    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_ver_denuncias);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //intancias
+
+    }
+}
