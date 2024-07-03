c3e45a84de01fe553100c2a6d330b4e47e1b72e7
Ariana <arifbottaro@gmail.com>
2024-07-03 12:57:10 -0300

    Cambios publicaciones

--- app/src/main/java/ar/edu/uade/municipio_frontend/Activities/Publicacion/CrearPublicacionActivity.java
+++ app/src/main/java/ar/edu/uade/municipio_frontend/Activities/Publicacion/CrearPublicacionActivity.java
@@ -12,10 +12,14 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -49,7 +53,17 @@ public class CrearPublicacionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_crear_publicacion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            return insets;
+
+        });
+
 
         inputTitulo = findViewById(R.id.inputTitulo);
         inputDescripcion = findViewById(R.id.inputDescripcion);
--- app/src/main/java/ar/edu/uade/municipio_frontend/Activities/Publicacion/VerPublicaciones.java
+++ app/src/main/java/ar/edu/uade/municipio_frontend/Activities/Publicacion/VerPublicaciones.java
@@ -13,8 +13,12 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import java.util.ArrayList;
@@ -56,7 +60,13 @@ public class VerPublicaciones extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ver_publicaciones);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         // Inicialización de vistas
         botonAgregar = findViewById(R.id.botonAgregar);
