bef38fbde2ad118c928199a1ccf155ed9dd0df33
Ariana <arifbottaro@gmail.com>
2024-07-03 14:32:43 -0300

    Cambios publicaciones parte 2

--- app/src/main/java/ar/edu/uade/municipio_frontend/Activities/Publicacion/CrearPublicacion.java
+++ app/src/main/java/ar/edu/uade/municipio_frontend/Activities/Publicacion/CrearPublicacion.java
@@ -18,7 +18,11 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -53,7 +57,17 @@ public class CrearPublicacion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_crear_publicacion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            return insets;
+
+        });
+
 
         titulo = findViewById(R.id.inputTitulo);
         descripcion = findViewById(R.id.inputDescripcion);
